b07ae1312f0b626b73af3b1ea3ea398dac126b0e
Shubham Ingole <52730862+shubhamneop@users.noreply.github.com>
2022-03-02 15:29:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzMY23pgK5RByUNpZW_Z0Ucizjue6hBHs",
+  apiKey: "key",
   authDomain: "signal-clone-f67cf.firebaseapp.com",
   projectId: "signal-clone-f67cf",
   storageBucket: "signal-clone-f67cf.appspot.com",
   messagingSenderId: "672379238512",
-  appId: "1:672379238512:web:4ecfc0de98be2a4f729cb5",
+  appId: "appid",
   measurementId: "G-KSVPRWY1VX",
 };
 let app;
